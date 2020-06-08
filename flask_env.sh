50f3db6558df38b93527f9968e3ceb90aa1b4523
jvansan <jeffreyavansanten@gmail.com>
2020-06-08 10:30:31 -0700

    Slack notifications for complete dataset

--- flask_env.sh
+++ flask_env.sh
@@ -3,3 +3,4 @@ export FLASK_CONFIG=development
 export FLASK_ENV=development
 export DBSERVER=127.0.0.1
 export REDIS=127.0.0.1
+export SLACK_WEBHOOK_URL=https://hooks.slack.com/services/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
